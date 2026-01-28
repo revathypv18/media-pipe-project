90bdf878072db7fddc17c9ccb31fd361eea1354d
revathy <revathypv18@gmail.com>
2026-01-28 23:40:56 +0530

    Added Streamlit app code

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="MediaPipe Project")
+
+st.title("🎯 MediaPipe Focus Project")
+st.write("This app is successfully connected to GitHub and Streamlit Cloud.")
+st.success("Deployment test successful!")
